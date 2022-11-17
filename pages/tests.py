399aa35e917101bbfb67dfe4fba473cf3af43f41
maksimcialehin <madqfrog1863@gmail.com>
2022-11-17 15:14:58 +0300

    'chapter6'

--- pages/tests.py
+++ pages/tests.py
@@ -1,7 +1,7 @@
 from django.test import SimpleTestCase
 from django.urls import reverse, resolve
 
-from .views import HomePageView
+from .views import HomePageView, AboutPageView
 
 
 class HomepageTests(SimpleTestCase):
@@ -17,7 +17,7 @@ class HomepageTests(SimpleTestCase):
         self.assertEqual(self.response.status_code, 200)
 
     def test_homepage_contains_correct_html(self):
-        self.assertContains(self.response, 'Homepage')
+        self.assertContains(self.response, 'Home')
 
     def test_homepage_does_not_contain_incorrect_html(self):
         self.assertNotContains(self.response, 'It should not be here')
@@ -27,4 +27,29 @@ class HomepageTests(SimpleTestCase):
         self.assertEqual(
             view.func.__name__,
             HomePageView.as_view().__name__
+        )
+
+
+class AboutpageTests(SimpleTestCase):
+    def setUp(self) -> None:
+        url = reverse('about')
+        self.response = self.client.get(url)
+
+    def test_aboutpage_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_aboutpage_url_name(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_aboutpage_contains_correct_html(self):
+        self.assertContains(self.response, 'About page')
+
+    def test_aboutpage_does_not_contain_incorrect_html(self):
+        self.assertNotContains(self.response, 'It should not be here')
+
+    def test_aboutpage_url_resolves_aboutpageview(self):
+        view = resolve('/about/')
+        self.assertEqual(
+            view.func.__name__,
+            AboutPageView.as_view().__name__
         )
--- pages/views.py
+++ pages/views.py
@@ -3,4 +3,8 @@ from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
     template_name: str = 'home.html'
+
+
+class AboutPageView(TemplateView):
+    template_name: str = 'about.html'
     
